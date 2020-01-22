402673b61b9b8fa7400ba596b3caa67e55ed9321
Santiago López <52461398+LopezSanty@users.noreply.github.com>
2020-01-21 18:20:47 -0600

    Se modifico enlistando el IV bimestre

--- resources/views/unidades/bimestrecuatro.blade.php
+++ resources/views/unidades/bimestrecuatro.blade.php
@@ -2,92 +2,208 @@
 
 @section('content')
 <div class="container">
-    <table class="table table-hover">
-        <thead>
-        <td colspan="5"><h2 style="text-align: center">Glifos mayas</h2></td>
-        </thead>
-        <tbody>
-        <tr>
-            <td width="50" height="50"><center> <img src="../../images/img-glifos/batz.jpg" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/e.jpg" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/iq.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/aqabal.png" height="200" width="200"></center>
-            </td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><h1 style="text-align: center">B'atz</h1><h3 style="text-align: center; color: #0E27ED">Mono</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">E</h1><h3 style="text-align: center; color: #0E27ED">Camino</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Iq'</h1><h3 style="text-align: center; color: #0E27ED">Viento</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Aq'ab'al</h1><h3 style="text-align: center; color: #0E27ED">Amanecer</h3></td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><center> <img src="../../images/img-glifos/kat.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/tzikin.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/tzi.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/tijax.jpg" height="200" width="200"></center>
-            </td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><h1 style="text-align: center">K'at</h1><h3 style="text-align: center; color: #0E27ED">Red</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Tz'ikin</h1><h3 style="text-align: center; color: #0E27ED">Pájaro</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Tz'i'</h1><h3 style="text-align: center; color: #0E27ED">Perro</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Tijax</h1><h3 style="text-align: center; color: #0E27ED">Piedra</h3></td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><center> <img src="../../images/img-glifos/ajmaq.jpg" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/keme.jpg" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/kan.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/imox.png" height="200" width="200"></center>
-            </td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><h1 style="text-align: center">Ajmaq</h1><h3 style="text-align: center; color: #0E27ED">Pecador</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Keme</h1><h3 style="text-align: center; color: #0E27ED">Tecolote</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Kan</h1><h3 style="text-align: center; color: #0E27ED">Movimiento</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Imox</h1><h3 style="text-align: center; color: #0E27ED">Pez</h3></td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><center> <img src="../../images/img-glifos/kej.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/qanil.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/toj.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/ajpu.jpg" height="200" width="200"></center>
-            </td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><h1 style="text-align: center">Kej</h1><h3 style="text-align: center; color: #0E27ED">Venado</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Q'anil</h1><h3 style="text-align: center; color: #0E27ED">Semilla</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Toj</h1><h3 style="text-align: center; color: #0E27ED">Ofrenda</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Ajpu</h1><h3 style="text-align: center; color: #0E27ED">Luz</h3></td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><center> <img src="../../images/img-glifos/kawoq.jpg" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/noj.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/ix.png" height="200" width="200"></center>
-            </td>
-            <td width="50" height="50"> <center> <img src="../../images/img-glifos/aj.jpg" height="200" width="200"></center>
-            </td>
-        </tr>
-        <tr>
-            <td width="50" height="50"><h1 style="text-align: center">Kawoq</h1><h3 style="text-align: center; color: #0E27ED">Relámpago</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">No'j</h1><h3 style="text-align: center; color: #0E27ED">Sabiduría</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Ix</h1><h3 style="text-align: center; color: #0E27ED">Madre Tierra</h3></td>
-            <td width="50" height="50"><h1 style="text-align: center">Aj</h1><h3 style="text-align: center; color: #0E27ED">Hogar</h3></td>
-        </tr>
-        </tbody>
-    </table>
+    <h1 style="color: #CF070A"><center>Glifos Mayas</center></h1>
+    <div class="row">
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/batz.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>B'atz</center></h1>
+                <h4><center>Mono</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/e.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>E</center></h1>
+                <h4><center>Camino</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Test1"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/iq.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Iq'</center></h1>
+                <h4><center>Viento</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/aqabal.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Aq'ab'al</center></h1>
+                <h4><center>Amanecer</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/kat.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>K'at</center></h1>
+                <h4><center>Red</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/tzikin.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Tz'ikin</center></h1>
+                <h4><center>Pájaro</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/tzi.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Tz'i'</center></h1>
+                <h4><center>Perro</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/tijax.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Tijax</center></h1>
+                <h4><center>Piedra</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/ajmaq.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Ajmaq</center></h1>
+                <h4><center>Pecador</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/keme.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Keme</center></h1>
+                <h4><center>Tecolote</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/kan.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Kan</center></h1>
+                <h4><center>Movimiento</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/imox.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Imox</center></h1>
+                <h4><center>Pez</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/kej.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Key</center></h1>
+                <h4><center>Venado</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/qanil.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Q'anil</center></h1>
+                <h4><center>Semilla</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/toj.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Toj</center></h1>
+                <h4><center>Ofrenda</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/ajpu.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Ajpu</center></h1>
+                <h4><center>Luz</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/kawoq.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Kawoq</center></h1>
+                <h4><center>Relámpago</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/noj.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>No'j</center></h1>
+                <h4><center>Sabiduría</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/ix.png"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Ix</center></h1>
+                <h4><center>Madre tierra</center></h4>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
+                   data-target="#image-gallery">
+                    <img class="img-thumbnail"
+                         src="../../images/img-glifos/aj.jpg"
+                         alt="Another alt text">
+                </a>
+                <h1><center>Aj</center></h1>
+                <h4><center>Hogar</center></h4>
+            </div>
+        </div>
 </div>
 @endsection
